3d8df9ea670ce484940b37a25f04e1dc6e1ee832
Chris Tyrrel <ChasedSpade@users.noreply.github.com>
2018-07-18 15:35:10 -0600

    Add PanicBNC Base Module (#18)

---
+++ modules/panicbnc.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2004-2018 PanicBNC, see the NOTICE file for details.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <znc/Modules.h>
+#include <znc/IRCNetwork.h>
+#include <znc/User.h>
+
+using std::map;
+using std::vector;
+
+class CPanicBNCMod : public CModule {
+    public:
+        MODCONSTRUCTOR(CPanicBNCMod) {
+            AddHelpCommand();
+            AddCommand("ListAllUserNetworks", "", "Lists all user networks.",
+                        [=](const CString& sLine) { ListAllUserNetworks(sLine); });
+        }
+
+        void ListAllUserNetworks(const CString& sLine) {
+            if (!GetUser()->IsAdmin()) {
+                PutModule("Access denied!");
+                return;
+            }
+            const map<CString, CUser*>& msUsers = CZNC::Get().GetUserMap();
+            for (const auto& it : msUsers) {
+                const vector<CIRCNetwork*>& vNetworks = it.second->GetNetworks();
+                for (const CIRCNetwork* pNetwork : vNetworks) {
+                    PutModNotice("LAUN " + it.first + " " + pNetwork->GetName() + " " +  CString(pNetwork->IsIRCConnected()) + " "
+                    + pNetwork->GetIRCServer() + " " + pNetwork->GetIRCNick().GetNickMask() + " " + pNetwork->GetBindHost()); 
+                }
+            }            
+        }
+};
+
+
+GLOBALMODULEDEFS(CPanicBNCMod, "PanicBNC Assistance Module")
